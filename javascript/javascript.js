346f975372704e3f99127cce1961d764a4f577c6
Georgia <ghobs987@gmail.com>
2021-06-09 00:56:29 +0100

    prelimary strength counter created

---
+++ javascript/javascript.js
@@ -0,0 +1,25 @@
+var a = document.getElementById("alpha").innerHTML;
+var b =10;
+var c = +a + b;
+document.getElementById("test").innerHTML= c;
+
+
+//  var d = document.getElementById("Strength").addEventListener("change",);
+var e =10;
+// var f= +d + e; 
+// document.getElementById("test2").innerHTML= f;
+
+
+function strcounter() {var x = document.getElementById("Strength").value;
+document.getElementById("test2").innerHTML = e + +x;}
+
+
+
+
+ document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+   document.getElementById("demo").innerHTML = Date();
+ }
+
+
