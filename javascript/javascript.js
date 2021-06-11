29c9d085b290e027b9c18ce7279504d8e4759454
Georgia <ghobs987@gmail.com>
2021-06-11 21:31:34 +0100

    race value counter created

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,18 +4,45 @@ var c = +a + b;
 document.getElementById("test").innerHTML= c;
 
 
-//  var d = document.getElementById("Strength").addEventListener("change",);
+const selectElement = document.querySelector('.races');
+
+selectElement.addEventListener('change', (event) => {
+console.log(event.target.value);
+});
+
 var e =10;
-// var f= +d + e; 
-// document.getElementById("test2").innerHTML= f;
-
-
-function strcounter() {var x = document.getElementById("Strength").value;
-document.getElementById("test2").innerHTML = e + +x;}
 
 
 
 
- 
+// the below works
+// function strcounter() {var x = document.getElementById("Strength").value;
+// document.getElementById("test2").innerHTML = e + +x;}
 
+// function dexcounter() {var q =document.getElementById("dex").value;
+// document.getElementById("test3").innerHTML = +q}
+
+var race=$('select#race').val();
+
+
+
+
+//  race value test working
+ function raceID () {console.log(race)
+//      var RI=
+    // document.getElementById("race").value;
+//     document.getElementById("test3").innerHTML = +q +RI;
+}
+
+
+// variables
+
+
+
+// race.addEventListener("click", Omega);
+
+
+// function Omega() {
+// var asdf =this.attributes["date-test"].value;
+// console.log("imperial");}
 
